713839ddcfbbe0934cc8ed0ec281566b842eb892
Jose Morales <jmoralesmnz@gmail.com>
2024-10-16 15:48:25 +0200

    fixed some styles base

--- src/styles/antdThemeConfig.ts
+++ src/styles/antdThemeConfig.ts
@@ -1,24 +1,25 @@
 const theme = {
   token: {
-    colorBgBase: "#E0E1DD", // Fondo principal claro
-    colorTextBase: "#0D1B2A", // Texto oscuro principal
-    colorPrimary: "#415A77", // Color primario para elementos interactivos
-    colorPrimaryBg: "#778DA9", // Fondo para elementos primarios
-    colorPrimaryBorder: "#1B263B", // Borde de elementos primarios
-    colorText: "#1B263B", // Color general del texto
-    colorTextPlaceholder: "#415A77", // Color del placeholder
+    //base
+    colorBgBase: "#E0E1DD",
+    colorTextBase: "#0D1B2A",
+    colorPrimary: "#415A77",
+    colorPrimaryBg: "#778DA9",
+    colorPrimaryBorder: "#1B263B",
+    colorText: "#1B263B",
+    colorTextPlaceholder: "#415A77",
 
-    // Colores específicos para inputs y selects
-    colorBgContainer: "#fff", // Fondo claro para inputs y selects
-    colorBgElevated: "#fff", // Fondo para inputs con elevación
-    colorBorder: "#415A77", // Borde de inputs y selects
-    colorBgContainerHover: "#778DA9", // Fondo al hacer hover sobre inputs
-
-    // Configuración para errores (botones danger)
-    colorError: "#FF4D4F", // Color para el botón danger
-    colorErrorBg: "#FF4D4F", // Fondo del botón danger
-    colorErrorBorder: "#FF4D4F", // Borde del botón danger
-    colorErrorHover: "#D9363E", // Rojo más oscuro al hacer hover
+    // selects and inputs
+    colorBgContainer: "#fff",
+    colorBgElevated: "#fff",
+    colorBorder: "#415A77",
+    colorBgContainerHover: "#778DA9",
+    colorTextSelected: "#0D1B2A",
+    //danger colors
+    colorError: "#FF4D4F",
+    colorErrorBg: "#FF4D4F",
+    colorErrorBorder: "#FF4D4F",
+    colorErrorHover: "#D9363E",
   },
 };
 
